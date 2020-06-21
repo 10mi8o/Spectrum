b29f5c7564aeaf33c3d0a16ebe816eb5e9d2986a
10mi8o <totoma855@gmail.com>
2020-06-21 10:39:39 +0900

    Contentfulからデータ取得できるようconfigファイルに設定を追記

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,5 +16,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST,
+      },
+    },
   ],
 }
